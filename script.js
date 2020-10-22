04e9f7e50e6b85ebacf4861b63b9ba73bc1c7a32
Sarah Wood <wood.sarah@outlook.com>
2020-10-22 12:47:23 +0100

    generateTarget function added

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget = () => {
-  return Math.floor((Math.random() * 9));
+  return Math.floor((Math.random() * 10));
 };
+
+console.log(generateTarget());
