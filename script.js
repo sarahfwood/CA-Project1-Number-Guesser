e98e2b12147c128533779f63a9fdc2c6dfa812cf
Sarah Wood <wood.sarah@outlook.com>
2020-10-22 14:22:00 +0100

    compareGuesses funciton updated

--- script.js
+++ script.js
@@ -11,15 +11,33 @@ const generateTarget = () => {
 console.log(generateTarget());
 
 const compareGuesses = (humanGuess, computerGuess, secretNumber) => {
-  if ((humanGuess - secretNumber) <= (computerGuess - secretNumber)) {
+  if (secretNumber > humanGuess) {
+    let guessOne = humanGuess - secretNumber;
+    console.log(guessOne);
+  } else {
+    secretNumber - humanGuess;
+  }
+
+
+
+
+
+  if ((humanGuess - secretNumber) >= (computerGuess - secretNumber)) {
     return true;
-  } else if ((humanGuess - secretNumber) > compareGuesses - secretNumber) {
+  } else if ((humanGuess - secretNumber) < compareGuesses - secretNumber) {
     return false;
   } else {
     return 'Invalid';
   }
 };
 
+
+
 console.log(compareGuesses(2, 2, 2));
 console.log(compareGuesses(1, 8, 2));
 console.log(compareGuesses(1, 8, 8));
+
+
+
+
+//logic: If secrert number is larger than guess 1 then takeaway otherwise just takeway 
