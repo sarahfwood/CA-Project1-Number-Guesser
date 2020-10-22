cc69ae3658c7413385005112610900867258c863
Sarah Wood <wood.sarah@outlook.com>
2020-10-22 13:31:23 +0100

    compareGuesses funciton updated

--- script.js
+++ script.js
@@ -13,9 +13,13 @@ console.log(generateTarget());
 const compareGuesses = (humanGuess, computerGuess, secretNumber) => {
   if ((humanGuess - secretNumber) <= (computerGuess - secretNumber)) {
     return true;
-  } else {
+  } else if ((humanGuess - secretNumber) > compareGuesses - secretNumber) {
     return false;
+  } else {
+    return 'Invalid';
   }
 };
 
-console.log(compareGuesses(2, 8, 3));
+console.log(compareGuesses(2, 2, 2));
+console.log(compareGuesses(1, 8, 2));
+console.log(compareGuesses(1, 8, 8));
